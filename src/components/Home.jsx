b91026f3b5d511a15b1f5721d5be13d1d5ada9b7
eclipssed <furqanmirzaig@gmail.com>
2023-12-23 15:06:14 +0500

    Added carousel of flowbite for better view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import { Carousel } from "flowbite-react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "flowbite-react";
+// import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
 import banner1 from "../assets/banner1.png";
 
 const Home = () => {
